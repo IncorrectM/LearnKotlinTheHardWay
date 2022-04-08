77e5e8e643809962f89bd978427b956f8778e76a
IncorrectM <1287365321@qq.com>
2022-04-08 22:50:45 +0800

    Ex 01 done

---
+++ src/ex02.kt
@@ -0,0 +1,14 @@
+/**
+ * 注释①
+ * @param args: 程序启动时。从命令行输入的指令
+ */
+fun main(args: Array<String>) {
+    /*
+    注释②
+    这里是注释
+    println("我不会被打印")
+    */
+    println("我会被打印")
+    // 注释③：println("我也不会被打印")
+    println("钝角")
+}
